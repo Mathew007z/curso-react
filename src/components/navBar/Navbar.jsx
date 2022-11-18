1af98d21fa4618f8aef22ee831725bee27c2049b
Mathew007z <fokker007@hotmail.com>
2022-11-18 11:08:57 -0300

    primer entrega

---
+++ src/components/navBar/Navbar.jsx
@@ -0,0 +1,32 @@
+
+import "./navbar.css";
+
+// Navbar
+
+
+
+
+ export function Navbar () { 
+
+  // Renderizando navbar
+
+  return <div className="navbar">
+
+      <ul className="navbar-ul">
+          <li>
+            <a href="" className="nav-link">Inicio</a>
+          </li>
+      </ul>
+      <ul className="navbar-ul">
+          <li>
+            <a href="" className="nav-link">Nosotros</a>
+          </li>
+      </ul>
+      <ul className="navbar-ul">
+          <li>
+            <a href="" className="nav-link">Productos</a>
+          </li>
+      </ul>
+
+  </div>
+}
